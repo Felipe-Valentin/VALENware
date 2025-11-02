3217fb16d68293d2e3e35c59888d1fd5cd2eef60
Felipe Valentin <186496450+Felipe-Valentin@users.noreply.github.com>
2025-11-02 12:10:04 -0300

    Update version and formatting in config file

--- Vtiny-maintainer-config.py
+++ Vtiny-maintainer-config.py
@@ -28,8 +28,9 @@ LZMA_compression = False
 ######################
 
 # Versão atual deste programa:
-version: "TESTING"
+version: "0.1-ALPHA"
 # Diretório da nova versão:
 new_version: "https://github.com/Felipe-Valentin/VALENware/releases/download/VALENtiny__v0.6-GAMA/Windows64_VALENtiny-Maintainer.exe"
+
 
 

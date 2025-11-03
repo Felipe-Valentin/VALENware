e90b9c6a052cca3c6445417c703928fab624b6d8
Felipe Valentin <186496450+Felipe-Valentin@users.noreply.github.com>
2025-11-03 19:23:42 -0300

    Update maintenance configuration parameters

--- Vtiny-maintainer-config.py
+++ Vtiny-maintainer-config.py
@@ -5,12 +5,12 @@
 ############################################
 
 # Tempo de vida útil do banco de dados, em horas:
-database_lifespan: 60
-# padrão: `database_lifespan: 60  # 2 dias e meio`
+database_lifespan: 8
+# padrão: `database_lifespan: 8`
 
 # Número máximo de cópias de segurança:
-max_backups: 48
-# padrão: `max-backups: 48  # 4 meses se database_lifespan = 60`
+max_backups: 360
+# padrão: `max-backups: 360  # 4 meses se database_lifespan = 8`
 
 # Diretório em que as cópias de segurança serão salvas:
 backup_path: "https://github.com/python-VALENtiny/VALENtiny/tree/main/BACKUPS"
@@ -28,9 +28,6 @@ LZMA_compression = False
 ######################
 
 # Versão atual deste programa:
-version: "0.1-ALPHA"
+version: "0.2-ALPHA"
 # Diretório da nova versão:
 new_version: "https://github.com/Felipe-Valentin/VALENware/releases/download/VALENtiny__v0.6-GAMA/Windows64_VALENtiny-Maintainer.exe"
-
-
-
